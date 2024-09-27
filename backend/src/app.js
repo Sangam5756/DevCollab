06e6a012ccf136ae2be942b154ccfd23a2af6ee2
sangam5756 <sangammunde3@gmail.com>
2024-09-27 08:15:08 +0530

    routeHandlers-how-it-work

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,35 @@ import express from "express";
 
 const app = express();
 
-app.get("/user/:id", (req, res) => {
-  // console.log(req.params)
-  // console.log(req.query)
-
-  res.send("Get user successfully");
-});
-app.post("/user", (req, res) => {   
-  res.send("post user successfully");
-});
-app.patch("/user", (req, res) => {
-  res.send("patch request successfully");
-});
-app.delete("/user", (req, res) => {
-  res.send("delete request successfully");
-});
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("response 1");
+      // res.send("response 1");
+      next();
+    },
+    (req, res, next) => {
+      console.log("response 2");
+      // res.send("response 2");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("response 3");
+    // res.send("response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("response 4");
+    res.send("response 4");
+    // next();
+  },
+  (req, res, next) => {
+    console.log("response 5");
+    res.send("response 5");
+  }
+);
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
