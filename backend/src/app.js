b61a5c402d6ac85ca078aaf088d05fc427f756d4
sangam5756 <sangammunde3@gmail.com>
2024-09-27 13:03:25 +0530

    db initialize

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,20 @@
 import express from "express";
 import dbConnect from "./config/database.js";
+import userModel from "./models/userSchema.js";
 
 const app = express();
 
+app.post("/signup", async (req, res) => {
+  const user = new userModel({
+    firstName: "Tushar",
+    lastName: "Shitole",
+    emailId: "tusharshitole@gmail.com",
+    password: "Secret@wsad",
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  await user.save();
+  res.send("Signup successFull");
+});
 
 dbConnect()
   .then(() => {
