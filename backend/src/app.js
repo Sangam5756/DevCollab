5cc94ff482932f13ee8cdb9552c828e2112ed3c1
sangam5756 <sangammunde3@gmail.com>
2024-09-27 11:40:36 +0530

    Remove env from tracking

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,37 @@
 import express from "express";
+import dbConnect from "./config/database.js";
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  // using try catch
-  try {
-    throw new Error("sdfdgdfg");
-    res.send("user all data");
-  } catch (err) {
-    res.status(404).send("err is occured contact support team");
-  }
-});
 
-// error handling using wild card for all
-app.use("/", (err, req, res, next) => {
-  res.send("something is wrong");
-});
 
-app.listen(5000, () => {
-  console.log("server is listening on port 5000");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dbConnect()
+  .then(() => {
+    console.log("Connected to db");
+
+    app.listen(5000, () => {
+      console.log("server is listening on port 5000");
+    });
+  })
+  .catch((err) => console.error("Connection to database failed" + err));
